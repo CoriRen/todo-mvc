811d1921b01d88bff2b0530706c0b15c03c9534b
Corinne GoodSmith <corinnegoodsmith@gmail.com>
2024-11-09 22:24:40 -0600

    added comments

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
+
+//find Mongoose connection string in config/database folder
 const connectDB = require('./config/database')
+
+//declare variables that store paths to routes folders
 const homeRoutes = require('./routes/home')
 const todoRoutes = require('./routes/todos')
 
+//Module to read .env file
 require('dotenv').config({path: './config/.env'})
 
+//call connectDB function and connect to Mongoose.
 connectDB()
 
 app.set('view engine', 'ejs')
@@ -13,9 +19,12 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+
+//listens to requests and sends them to the proper routes. 
 app.use('/', homeRoutes)
 app.use('/todos', todoRoutes)
  
+
 app.listen(process.env.PORT, ()=>{
     console.log('Server is running, you better catch it!')
 })    
